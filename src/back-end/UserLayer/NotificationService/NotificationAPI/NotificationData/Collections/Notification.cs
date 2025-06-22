2ff0a994c4e694e41e77ff73a03d93666448bcdc
Ath21 <billath131908@gmail.com>
2025-06-22 13:45:59 +0300

    3.4 Notification Collection

---
+++ src/back-end/UserLayer/NotificationService/NotificationAPI/NotificationData/Collections/Notification.cs
@@ -0,0 +1,28 @@
+using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace NotificationData.Collections;
+
+public class Notification
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+
+    [BsonElement("type")]
+    public string Type { get; set; }
+
+    [BsonElement("recipientId")]
+    public string RecipientId { get; set; }
+
+    [BsonElement("message")]
+    public string Message { get; set; }
+
+    [BsonElement("status")]
+    public string Status { get; set; }
+
+    [BsonElement("timestamp")]
+    public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+
+}
