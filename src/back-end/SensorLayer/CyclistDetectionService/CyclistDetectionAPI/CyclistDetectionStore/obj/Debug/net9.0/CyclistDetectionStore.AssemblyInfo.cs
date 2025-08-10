0dfe5fee221787e6748d0a88e0a3e3ce7b49c6f7
Ath21 <billath131908@gmail.com>
2025-08-10 14:01:09 +0300

    9.3 Build folders of Store Web API

--- src/back-end/SensorLayer/CyclistDetectionService/CyclistDetectionAPI/CyclistDetectionStore/obj/Debug/net9.0/CyclistDetectionStore.AssemblyInfo.cs
+++ src/back-end/SensorLayer/CyclistDetectionService/CyclistDetectionAPI/CyclistDetectionStore/obj/Debug/net9.0/CyclistDetectionStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CyclistDetectionStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d7e4f58bfce9f5851f9cf64fc046a0b5af3c556")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ae00383f4741592489bd7a271cf015d8859803b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CyclistDetectionStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CyclistDetectionStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
