5008b8c1c986adfa35b31b87774c01d989a4448e
Ath21 <billath131908@gmail.com>
2025-08-10 18:48:15 +0300

    10.1 Initialize project

--- src/back-end/SensorLayer/SensorMessages/obj/Debug/net9.0/SensorMessages.AssemblyInfo.cs
+++ src/back-end/SensorLayer/SensorMessages/obj/Debug/net9.0/SensorMessages.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SensorMessages")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3f53b539d9c20c97d8c49015630dfe7d3ee44b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c261e2078d7011cbbb0beb313659f40ff0bc457")]
 [assembly: System.Reflection.AssemblyProductAttribute("SensorMessages")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SensorMessages")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
