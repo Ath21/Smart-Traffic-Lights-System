d2b92807265f627e6e165d4bdb9f337dc33c6c16
Ath21 <billath131908@gmail.com>
2025-09-01 10:30:17 +0300

    19.12 Update dockerfile

--- src/back-end/TrafficLayer/TrafficLightCoordinatorService/TrafficLightCoordinatorAPI/TrafficLightCoordinatorStore/obj/Debug/net9.0/TrafficLightCoordinatorStore.AssemblyInfo.cs
+++ src/back-end/TrafficLayer/TrafficLightCoordinatorService/TrafficLightCoordinatorAPI/TrafficLightCoordinatorStore/obj/Debug/net9.0/TrafficLightCoordinatorStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrafficLightCoordinatorStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61f08731389d9bf272a9940ddd34f50c93d48e17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+869f38cdcd0915e445be62a83cc74c86b337ee58")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrafficLightCoordinatorStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrafficLightCoordinatorStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/back-end/TrafficLayer/TrafficMessages/obj/Debug/net9.0/TrafficMessages.AssemblyInfo.cs
+++ src/back-end/TrafficLayer/TrafficMessages/obj/Debug/net9.0/TrafficMessages.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrafficMessages")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61f08731389d9bf272a9940ddd34f50c93d48e17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+869f38cdcd0915e445be62a83cc74c86b337ee58")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrafficMessages")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrafficMessages")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
