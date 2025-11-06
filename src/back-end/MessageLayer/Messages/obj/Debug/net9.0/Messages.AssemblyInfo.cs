90c3d00dd04f785c14de2e2e542667632849b08f
Ath21 <billath131908@gmail.com>
2025-11-07 01:37:02 +0200

    to fix admin and operator dashboard

--- src/back-end/MessageLayer/Messages/obj/Debug/net9.0/Messages.AssemblyInfo.cs
+++ src/back-end/MessageLayer/Messages/obj/Debug/net9.0/Messages.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Messages")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+207bb8eb7ddf8cd2afc739e12626a1f64c84c6af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3e00d6ce54072ba9336d4756d534f7d7f11100c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Messages")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Messages")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/back-end/UserLayer/Databases/UserDB/UserData/obj/Debug/net9.0/UserData.AssemblyInfo.cs
+++ src/back-end/UserLayer/Databases/UserDB/UserData/obj/Debug/net9.0/UserData.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserData")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+207bb8eb7ddf8cd2afc739e12626a1f64c84c6af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3e00d6ce54072ba9336d4756d534f7d7f11100c")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserData")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserData")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
